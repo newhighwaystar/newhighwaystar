f30c0273fa7b8cc53e7169bb57475901a4194999
newhighwaystar <62964702+newhighwaystar@users.noreply.github.com>
2020-04-04 00:06:20 -0700

    Add files via upload

---
+++ js/main-test.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+  $('.header').height($(window).height());
+  
+   $("a").click(function(event) {
+    var href = $(event.target).attr('href').substring(1); //this will give you the href value
+	
+	if(href=="about")
+	{
+		  document.getElementById(href).style.display = "block";
+          document.getElementById("home").style.display = "none";
+          document.getElementById("contact").style.display = "none";
+
+    }
+ 
+    else if(href=="contact")
+	{
+          document.getElementById(href).style.display = "block";
+          document.getElementById("home").style.display = "none";
+          document.getElementById("about").style.display = "none";    }
+	
+	else if(href=="home")
+	{
+          document.getElementById(href).style.display = "block";
+          document.getElementById("about").style.display = "none";
+          document.getElementById("contact").style.display = "none";
+    }
+   });
+});
